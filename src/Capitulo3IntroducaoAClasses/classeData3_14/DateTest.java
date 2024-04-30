735f321cdd4a571ce60de0ec198ee170ab8726fa
Jorge Gabriel <jorge85x@gmail.com>
2024-04-29 23:18:11 -0300

    Questão resolvida do capítulo 3.

---
+++ src/Capitulo3IntroducaoAClasses/classeData3_14/DateTest.java
@@ -0,0 +1,9 @@
+package Capitulo3IntroducaoAClasses.classeData3_14;
+
+public class DateTest {
+
+    public static void main(String[] args) {
+        Date test = new Date(8, 28, 2005);
+        test.displayDate();
+    }
+}
