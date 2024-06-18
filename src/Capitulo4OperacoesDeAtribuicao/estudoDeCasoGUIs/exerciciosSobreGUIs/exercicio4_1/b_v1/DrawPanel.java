0d777e077caf32d5b1d58eeab4732abd6dddf18b
Jorge Gabriel <jorge85x@gmail.com>
2024-06-17 23:48:17 -0300

    questão resolvida do capítulo 4 de GUI.

--- src/Capitulo4OperacoesDeAtribuicao/estudoDeCasoGUIs/exerciciosSobreGUIs/exercicio4_1/b_v1/DrawPanel.java
+++ src/Capitulo4OperacoesDeAtribuicao/estudoDeCasoGUIs/exerciciosSobreGUIs/exercicio4_1/b_v1/DrawPanel.java
@@ -19,22 +19,6 @@ public class DrawPanel extends JPanel {
             y2 += 33;
             x2 -= 30;
         }
-        //right
-        x2 = 48;
-        y2 = 25;
-        for (int i = 0; i < 13; i++) {
-            g.drawLine(width, 0, x2, y2);
-            y2 += 33;
-            x2 += 30;
-        }
-        //left bottom
-        x2 = 48;
-        y2 = 25;
-        for (int i = 0; i < 13; i++) {
-            g.drawLine(0, height, x2, y2);
-            x2 += 33;
-            y2 += 32;
-        }
         //right bottom
         x2 = 48;
         y2 = height-27;
@@ -43,24 +27,23 @@ public class DrawPanel extends JPanel {
             y2 -= 33;
             x2 += 30;
         }
+        //right
+        x2 = 48;
+        y2 = 25;
+        for (int i = 0; i < 13; i++) {
+            g.drawLine(width, 0, x2, y2);
+            y2 += 32;
+            x2 += 30;
+        }
+        //left bottom
+        x2 = 48;
+        y2 = 25;
+        for (int i = 0; i < 13; i++) {
+            g.drawLine(0, height, x2, y2);
+            y2 += 32;
+            x2 += 30;
+        }
 
-/*        int passos = 0;
 
-
-        while( height - passos >= 0){
-
-            //canto superior esquerdo
-            g.drawLine(0, 0, passos, height - passos);
-
-            //canto inferior direito
-            g.drawLine(width, height, passos, height - passos);
-
-            //canto superior direito
-            g.drawLine(width, 0, passos, passos);
-
-            //canto inferior esquerdo
-            g.drawLine(0, height, passos, passos);
-
-            passos = passos + 15;}*/
     }
 }
